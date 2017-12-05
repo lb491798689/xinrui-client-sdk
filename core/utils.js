32bd47f8526d3312881937c1f985db6a346ef413
qdxinrui <tangxinzhang@126.com>
2017-12-05 16:26:25 +0800

    测试

--- core/utils.js
+++ core/utils.js
@@ -4,7 +4,6 @@
  */
 exports.extend = function extend(target) {
     var sources = Array.prototype.slice.call(arguments, 1);
-
     for (var i = 0; i < sources.length; i += 1) {
         var source = sources[i];
         for (var key in source) {
--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 var constants = require('./core/constants');
-var login = require('./core/login');
+var login   = require('./core/login');
+var Session = require('./core/session');
+var request = require('./core/request'); 
 
 var exports = module.exports = {
-    login: login.login
+    login: login.login,
+    setLoginUrl: login.setLoginUrl,
+    LoginError: login.LoginError,
+    clearSession: Session.clear,
+    request: request.request,
+    buildSessionHeader: request.buildSessionHeader,
+    RequestError: request.RequestError,
+    //Tunnel: Tunnel,
 };
+
+// 导出错误类型码
+Object.keys(constants).forEach(function(key){
+    if(key.indexOf('ERR_') === 0){
+        exports[key] = constants[key];
+    }
+});
