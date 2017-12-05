39946c3a654b262228d621f1645a03107464b847
qdxinrui <tangxinzhang@126.com>
2017-12-05 13:00:03 +0800

    测试

---
+++ core/login.js
@@ -0,0 +1,16 @@
+var utils = require('./utils');
+var constants = require('./constants');
+var Session = require('./session');
+
+
+var login = function login()
+{
+    console.log("test");
+}
+
+
+module.exports = {
+    //LoginError: LoginError,
+    login: login,
+    //setLoginUrl: setLoginUrl,
+};
---
+++ index.js
@@ -0,0 +1,6 @@
+var constants = require('./core/constants');
+var login = require('./core/login');
+
+var exports = module.exports = {
+    login: login.login
+};
